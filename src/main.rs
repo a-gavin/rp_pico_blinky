4a398c6f5a145603f5f61384fdb6a6da196d400a
A Gavin <a_gavin@icloud.com>
2022-09-01 15:21:34 -0700

    Add debounce via alarm for button

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,8 @@
 use cortex_m::delay::Delay;
 use cortex_m_rt::entry;
 use embedded_hal::digital::v2::ToggleableOutputPin;
+use fugit::MillisDurationU32;
 
-use rp_pico as bsp;
 use bsp::hal::{
     clocks::{init_clocks_and_plls, ClockSource},
     gpio::pin::bank0::{Gpio16, Gpio18, Gpio25},
@@ -17,9 +17,10 @@ use bsp::hal::{
     pac,
     pac::interrupt,
     sio::Sio,
-//    timer::{Alarm0, Timer},
+    timer::{Alarm, Alarm0, Timer},
     watchdog::Watchdog,
 };
+use rp_pico as bsp;
 
 use defmt::*;
 use defmt_rtt as _;
@@ -28,13 +29,16 @@ use panic_probe as _;
 use core::cell::RefCell;
 use critical_section::Mutex;
 
+const DEBOUNCE_DELAY: MillisDurationU32 = MillisDurationU32::millis(50);
+
 struct InterruptPeripherals {
     led_pin: Pin<Gpio25, PushPullOutput>,
     button_pin: Pin<Gpio16, PullUpInput>,
     pulse_pin: Pin<Gpio18, PushPullOutput>,
-    //alarm: Alarm0,
+    alarm: Alarm0,
 }
-static INTERRUPT_PERIPHERALS: Mutex<RefCell<Option<InterruptPeripherals>>> = Mutex::new(RefCell::new(None));
+static INTERRUPT_PERIPHERALS: Mutex<RefCell<Option<InterruptPeripherals>>> =
+    Mutex::new(RefCell::new(None));
 
 pub struct PicoDevice {
     delay: Delay,
@@ -66,13 +70,10 @@ impl PicoDevice {
         .unwrap();
 
         // Init delay struct
-        let delay: Delay = Delay::new(
-            core.SYST, 
-            clocks.system_clock.get_freq().to_Hz()
-        );
+        let delay: Delay = Delay::new(core.SYST, clocks.system_clock.get_freq().to_Hz());
 
-        //let mut timer = Timer::new(pac.TIMER, &mut pac.RESETS);
-        //let alarm = timer.alarm_0().unwrap();
+        let mut timer = Timer::new(pac.TIMER, &mut pac.RESETS);
+        let mut alarm = timer.alarm_0().unwrap();
 
         let pins = bsp::Pins::new(
             pac.IO_BANK0,
@@ -86,30 +87,31 @@ impl PicoDevice {
         let pulse_pin = pins.gpio18.into_push_pull_output();
         let button_pin = pins.gpio16.into_pull_up_input();
 
-        // Enable interrupt for sw1 pin (won't trigger until bank enabled)
+        // Enable interrupt for button pin and alarm (won't trigger until bank enabled)
         button_pin.set_interrupt_enabled(EdgeLow, true);
+        alarm.enable_interrupt();
 
         // Transfer ownership of pins into `GLOBAL_PINS` variable
         // for sole use by the interrupt handler.
-        let interrupt_periphs = InterruptPeripherals{
+        let interrupt_periphs = InterruptPeripherals {
             led_pin,
             button_pin,
             pulse_pin,
-            //alarm: alarm,
+            alarm,
         };
         critical_section::with(|cs| {
-            INTERRUPT_PERIPHERALS.borrow(cs).replace(Some(interrupt_periphs));
+            INTERRUPT_PERIPHERALS
+                .borrow(cs)
+                .replace(Some(interrupt_periphs));
         });
 
         // Enable interrupts for IO_IRQ_BANK0
         unsafe {
             pac::NVIC::unmask(pac::Interrupt::IO_IRQ_BANK0);
-            //pac::NVIC::unmask(pac::Interrupt::TIMER_IRQ_0);
+            pac::NVIC::unmask(pac::Interrupt::TIMER_IRQ_0);
         }
 
-        PicoDevice {
-            delay,
-        }
+        PicoDevice { delay }
     }
 
     fn delay_ms(&mut self, ms: u32) {
@@ -135,21 +137,41 @@ fn IO_IRQ_BANK0() {
         let mut interrupt_periphs = INTERRUPT_PERIPHERALS.borrow(cs).take();
 
         if let Some(mut periphs) = interrupt_periphs {
-
             if periphs.button_pin.interrupt_status(EdgeLow) {
-                _ = periphs.led_pin.toggle();
-                _ = periphs.pulse_pin.toggle();
+                if periphs.alarm.finished() {
+                    _ = periphs.led_pin.toggle();
+                    _ = periphs.pulse_pin.toggle();
+                    _ = periphs.alarm.schedule(DEBOUNCE_DELAY);
+                }
+
                 periphs.button_pin.clear_interrupt(EdgeLow);
             }
 
             interrupt_periphs = Some(periphs);
         }
 
-        INTERRUPT_PERIPHERALS.borrow(cs).replace_with(|_| interrupt_periphs);
+        INTERRUPT_PERIPHERALS
+            .borrow(cs)
+            .replace_with(|_| interrupt_periphs);
     });
 }
 
-//#[interrupt]
-//fn TIMER_IRQ_0() {
-//
-//}
+#[interrupt]
+fn TIMER_IRQ_0() {
+    critical_section::with(|cs| {
+        // Temporarily take peripherals
+        let mut interrupt_periphs = INTERRUPT_PERIPHERALS.borrow(cs).take();
+
+        if let Some(mut periphs) = interrupt_periphs {
+            if periphs.alarm.finished() {
+                periphs.alarm.clear_interrupt();
+            }
+
+            interrupt_periphs = Some(periphs);
+        }
+
+        INTERRUPT_PERIPHERALS
+            .borrow(cs)
+            .replace_with(|_| interrupt_periphs);
+    });
+}
